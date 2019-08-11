cf4c4995044f7ccc230a8f23a9ec81b6d39b2c59
timothydowd <timdowd1984@gmail.com>
2019-08-11 22:07:10 +0100

    added buttons for folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React, { Component } from 'react'
-import data from './data/data.json'
+import React, { Component } from "react";
+import data from "./data/data.json";
 
 export default class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       data: data
-    }
+    };
   }
+
+  handleClickFolderButton(){
+    console.log('click open folder')
+  }
+
   render() {
     return (
       <ul>
         {this.state.data.map(item => {
-          return(
-            <li key={item.name}>name: {item.name} - type: {item.type} - added: {item.added}</li>
-          )
+          if (item.type === "folder") {
+            return (
+              <li key={item.name}>
+                name: {item.name} - type: {item.type}{" "}
+                <button onClick={ this.handleClickFolderButton }>open/close folder</button>
+              </li>
+            );
+          } else {
+            return (
+              <li key={item.name}>
+                name: {item.name} - type: {item.type} - added: {item.added}
+              </li>
+            );
+          }
         })}
-        
       </ul>
-    )
+    );
   }
 }
+
 
