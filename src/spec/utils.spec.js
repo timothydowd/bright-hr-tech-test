c17624da98e6c6b889a094b32d17eada3010915a
timothydowd <timdowd1984@gmail.com>
2019-08-11 23:22:07 +0100

    sortData basic sort functionality working on array containg 2 objects with type criteria - tested

--- src/spec/utils.spec.js
+++ src/spec/utils.spec.js
@@ -4,11 +4,59 @@ const deepFreeze = require('deep-freeze')
 
 
 describe('sortData', () => {
-    it('when given an empty array of data and a criteria returns an empty array', () => {
+    it('when given an empty array of data and a criteria of "type" returns an empty array', () => {
         const data = []
         const criteria = 'type'
         deepFreeze(data)
         deepFreeze(criteria)
         expect(sortData(criteria, data)).to.eql([]) 
     });
+
+    it('when given an array of data containing 1 object and a criteria of "type", returns the array with 1 object', () => {
+    const data = [
+        {
+            "type": "pdf",
+            "name": "Employee Handbook",
+            "added": "2017-01-06"
+        }
+    ]
+    const criteria = 'type'
+    deepFreeze(data)
+    deepFreeze(criteria)
+    expect(sortData(criteria, data)).to.eql(data)
+    });
+
+    it('when givven ana array of data containing 2 unordered objects by a criteria of type, returns an array with the objecs in order of type', () => {
+        const data = [
+            {
+                "type": "pdf",
+                "name": "Employee Handbook",
+                "added": "2017-01-06"
+            },
+            {
+                "type": "csv",
+                "name": "Cost centres",
+                "added": "2016-08-12"
+            }
+        ]
+        const criteria = 'type'
+        const expected = [
+            {
+                "type": "csv",
+                "name": "Cost centres",
+                "added": "2016-08-12"
+            },
+            {
+                "type": "pdf",
+                "name": "Employee Handbook",
+                "added": "2017-01-06"
+           }
+        ]
+        deepFreeze(data)
+        deepFreeze(criteria)
+        expect(sortData(criteria, data)).to.eql(expected)
+    });
+
+
 });
+
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,17 @@
 const sortData = (criteria, data) => {
-    if(data.length === 0) return []
+    if(data.length === 0 || data.length === 1) return data
+
+    const sortedData = [...data].sort((a, b) => {
+        if(a[criteria] < b[criteria]){
+            return -1
+        } else if (a[criteria] > b[criteria]){
+            return 1
+        } else {
+            return 0
+        }
+    })
+
+    return sortedData
 }
 
 module.exports = { sortData }
