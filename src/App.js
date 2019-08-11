21fbb966e6b4224aac9764d1eaeb60e1c34040cd
timothydowd <timdowd1984@gmail.com>
2019-08-11 21:50:47 +0100

    json contents mapped and rendered, showing on screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import data from './data/data.json'
 
 export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: data
+    }
+  }
   render() {
     return (
-      <div>
+      <ul>
+        {this.state.data.map(item => {
+          return(
+            <li key={item.name}>name: {item.name} - type: {item.type} - added: {item.added}</li>
+          )
+        })}
         
-      </div>
+      </ul>
     )
   }
 }
